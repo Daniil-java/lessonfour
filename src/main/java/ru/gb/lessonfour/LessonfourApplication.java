067acb41e7d6177770b1cf9105632118c6b6a93d
ПК <saponembigas@gmail.com>
2022-10-03 16:39:25 +0600

    Initial commit

---
+++ src/main/java/ru/gb/lessonfour/LessonfourApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.lessonfour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonfourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonfourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/lessonfour/LessonfourApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.lessonfour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonfourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
